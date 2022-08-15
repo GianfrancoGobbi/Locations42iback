0e6702a2a00588f2d0daec57f678a9ee51a09e5b
Gianfranco Gobbi <93214155+GianfrancoGobbi@users.noreply.github.com>
2022-08-15 16:51:38 -0300

    Update app.js

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const app = express()
 const cors = require("cors")
 const router = require('./routes')
 
+app.use(cors({ origin: 'https://42i.netlify.app/' }));
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', 'https://42i.netlify.app/'); // update to match the domain you will make the request from
     res.header('Access-Control-Allow-Credentials', 'true');
@@ -13,5 +14,5 @@ app.use((req, res, next) => {
   });
 app.use(express.json())
 app.use(router)
-app.use(cors())
+
 module.exports = app
