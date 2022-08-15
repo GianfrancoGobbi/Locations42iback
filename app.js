76cadc5b5b5f904aba99a78e356687ae391eb804
Gianfranco Gobbi <93214155+GianfrancoGobbi@users.noreply.github.com>
2022-08-15 16:43:40 -0300

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const cors = require("cors")
 const router = require('./routes')
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // update to match the domain you will make the request from
+    res.header('Access-Control-Allow-Origin', 'https://42i.netlify.app/'); // update to match the domain you will make the request from
     res.header('Access-Control-Allow-Credentials', 'true');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
